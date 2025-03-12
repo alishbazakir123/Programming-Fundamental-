fc8f0ae2a0fe2efa6dd136cc8dc6818f66bffaf9
Alishba Zakir Janjua <alishbazakirjanjua@gmail.com>
2025-03-12 07:45:51 +0500

    Add files via upload

---
+++ Alish lab 11.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//	int a[5]={1,2,3,4,5};
+//	for(int i=0;i<5;i++){
+//		cout<<a[i];
+//	}	
+	
+	
+	
+//	      #02
+
+
+
+int n;
+cout<<"ENTER THE VALUE OF N:";
+cin>>n;
+int arr[n];
+cout<<"Enter array :";
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+}cout<<"Reverse of an array is :";
+for(int i=4;i>=0;i--){
+	cout<<arr[i]<<"  ";
+}
+
+
+
+
+
+
+//      #03
+
+//int a[5]={10,15,20,25,30};
+//cout<<"Array element ";
+//int sum=0;
+//for(int i=o;i<5;i++){
+//	sum+=a[i];
+//}
+//cout<<"sum :"<<sum<<endl;
+//double average=sum/5;
+//	cout<<"Average of element :"<<average;
+//}
+
+
+
+
+
+//             #04
+//int arr[]={10,20,30,40};
+//int size=sizeof(arr)/sizeof arr[0];
+//cout<<"size of the arry is :"<<size<<endl;
+//for(int i=0;i<size;i++){
+//	cout<<arr[i]<<"  ";
+//}
+
+
+
+
+//      #05
+//int n;
+//cout<<"Enter the size : ";
+//int arr[n];
+//int largest =0;
+//for(int i=0;i<n;i++){
+//	cin>>arr[i];
+//	if (arr[i]>largest){
+//		largest=arr[i];
+//	}
+//}cout<<"largest number is = "<<largest;
+
+
+return 0;
+
+
+
+
+
+
+
+	
+}
